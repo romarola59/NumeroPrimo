b46202c226580b50c0cb27c5c2f19430d99034c5
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-10 00:56:41 -0300

    Numeroprimo

--- Primo/Program.cs
+++ Primo/Program.cs
@@ -2,11 +2,28 @@
 
 namespace Primo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Primo(int n)
         {
-            Console.WriteLine("Hello World!");
+            int cont = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0) cont++;
+            }
+            if (cont == 2)
+                Console.WriteLine("Este es un numero primo");
+            else
+                Console.WriteLine("Este no es un numero primo");
+        }
+        public static void Main()
+        {
+            int n;
+            Console.WriteLine("Ingrese un numero primo ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Primo(n);
+
+            Console.ReadKey();
         }
     }
 }
